797ebc9b2e3e64ae8d1e8af4977f4f72d42482e7
Felipe-Perides <f.nicolau@edu.pucrs.br>
2024-08-13 15:42:58 -0300

    Quarto Commit(Implementei barras de vida, aprimorei o sistema de turnos e coloquei um sistema de rodadas gráfico)

--- Simulador_Lutas/src/simulador_Interface/Batalha.java
+++ Simulador_Lutas/src/simulador_Interface/Batalha.java
@@ -1,10 +1,13 @@
 package simulador_Interface;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.util.Random;
 import simulador_Interface.Criacao_Personagem;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JButton;
@@ -16,11 +19,14 @@ import javax.swing.JTextField;
 public class Batalha extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	private JPanel contentPane, panel;
 	private Criacao_Personagem person;
-	private int roundLucky, playerLucky;
+	private Random rand = new Random();
+	private int roundLucky=rand.nextInt(6)+1, playerLucky=rand.nextInt(2)+1, round;
 	private int attk1S,attk2S,def1S,def2S,vida1S,vida2S;
 	private JTextField textField;
+	private JButton proximo;
+	private JProgressBar vidaBar1, vidaBar2;
 
 	/**
 	 * Launch the application.
@@ -46,10 +52,50 @@ public class Batalha extends JFrame {
 	public Batalha(Criacao_Personagem person) {
 		this.person=person;
 		initComponents();
+		round = 0;
+		panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setFont(new Font("Arial", Font.BOLD, 20));
+                g.drawString("Turno: " + round, getWidth() / 2 - 50, 30);
+            }
+            };
+            panel.setBounds(122, 31, 236, 59);
+	 	    getContentPane().add(panel);
+	 	    
+	 	   vidaBar1 = new JProgressBar();
+	        vidaBar1.setMaximum(20);
+	        vidaBar1.setValue(vida1S);
+	        vidaBar1.setStringPainted(true);
+	        vidaBar1.setBounds(10, 211, 150, 30);
+	        contentPane.add(vidaBar1);
+
+	        vidaBar2 = new JProgressBar();
+	        vidaBar2.setMaximum(20);
+	        vidaBar2.setValue(vida2S);
+	        vidaBar2.setStringPainted(true);
+	        vidaBar2.setBounds(374, 211, 150, 30);
+	        contentPane.add(vidaBar2);
+	 	    
+	 	    proximo = new JButton("PRÓXIMO TURNO");
+	 	    proximo.addActionListener(new ActionListener() {
+	 	    	public void actionPerformed(ActionEvent e) {
+	 	    		round++;
+	                simularBatalha();
+	                panel.repaint();
+	                vidaBar1.setValue(vida1S);
+	                vidaBar2.setValue(vida2S);
+	 	    	}
+	 	    });
+	 	    proximo.setBounds(177, 263, 154, 23);
+	 	    contentPane.add(proximo);
+	 	    proximo.setVisible(false);
+	 	    panel.setVisible(false);
 	}
 	
+	
 	public void initComponents() {
-		Random rand = new Random();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 550, 400);
 		contentPane = new JPanel();
@@ -90,10 +136,12 @@ public class Batalha extends JFrame {
 		comecar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				comecar.setVisible(false);
+				proximo.setVisible(true);
+				panel.setVisible(true);
 				initBatalha();
 			}
 		});
-		comecar.setBounds(199, 245, 112, 23);
+		comecar.setBounds(198, 230, 112, 23);
 		contentPane.add(comecar);
 		
 		attk1S=person.getAttk1();attk2S=person.getAttk2();def1S=person.getDef1();def2S=person.getDef2();vida1S=person.getVida1();vida2S=person.getVida2();
@@ -115,47 +163,43 @@ public class Batalha extends JFrame {
 		contentPane.add(atq2);
 		
 		textField = new JTextField(" ");
-		textField.setBounds(127, 91, 271, 83);
+		textField.setBounds(111, 116, 271, 83);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		textField.setVisible(false);
 		
-		
-		//Dado + moeda
-		playerLucky = rand.nextInt(2)+1;
-		roundLucky = rand.nextInt(6)+1;
 	}
 	
 	public void initBatalha() {
 		attk1S=person.getAttk1();attk2S=person.getAttk2();def1S=person.getDef1();def2S=person.getDef2();vida1S=person.getVida1();vida2S=person.getVida2();
-		int ataq1 = attk1S, ataq2 = attk2S, defe1 = def1S, defe2 = def2S, resto;
-		// i == round
-		while(vida1S>0 && vida2S>0) {
-			for(int i = 0; i<20; i++) {
-				System.out.println("Rodada " + i + ": Vida Jogador 1 = " + vida1S + ", Vida Jogador 2 = " + vida2S);
-				if(i%2==0) { // vez do Jogador 2
-					if(ataq2<defe1) {
-						defe1-=ataq2;
-					}else {
-						resto = ataq2-defe1;
-						vida1S -= resto;
-					}
-				}else {  // vez do Jogador 1
-					if(ataq1<defe2) { 
-						defe2-=ataq1;
-					}else {
-						resto = ataq1-defe2;
-						vida2S -= resto;
-					}
-				}
-			}
-		}
-		if(vida1S<=0) {
-			textField.setText("Parabéns Jogador 2! Você venceu!");
-			textField.setVisible(true);
-		}else if(vida2S<=0) {
-			textField.setText("Parabéns Jogador 1! Você venceu!");
-			textField.setVisible(true);
-		}
 	}
+	private void simularBatalha() {
+        int ataq1 = attk1S, ataq2 = attk2S, defe1 = def1S, defe2 = def2S, resto;
+
+        if (round % 2 == 0) { // vez do Jogador 2
+            if (ataq2 < defe1) {
+                defe1 -= ataq2;
+            } else {
+                resto = ataq2 - defe1;
+                vida1S -= resto;
+            }
+        } else { // vez do Jogador 1
+            if (ataq1 < defe2) {
+                defe2 -= ataq1;
+            } else {
+                resto = ataq1 - defe2;
+                vida2S -= resto;
+            }
+        }
+
+        if (vida1S <= 0) {
+            textField.setText("Parabéns Jogador 2! Você venceu!");
+            textField.setVisible(true);
+            proximo.setEnabled(false);
+        } else if (vida2S <= 0) {
+            textField.setText("Parabéns Jogador 1! Você venceu!");
+            textField.setVisible(true);
+            proximo.setEnabled(false);
+        }
+    }
 }
